cb36c9295b9186b1c13cbb1884392ad765b10acb
alanmrocha <alanmrochadeveloper@gmail.com>
2021-04-25 19:35:12 -0300

    added cartIcon and cartDropdown components and implemented cartReducer2

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CustomButton from '../custom-button/custom-button.component';
 import './collection-item.styles.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
@@ -10,7 +11,8 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
 		<div className='collection-footer'>
 			<span className='name'>{name}</span>
 			<span className='price'>{price}</span>
-		</div> 
+		</div>
+		<CustomButton inverted>Add to cart</CustomButton>
 	</div>
 );
 
